0b7bd55418b951d07a7cad5eea9bdc6f570f5845
LordAldi <aldianugra09@gmail.com>
2020-07-03 18:05:26 +0700

    another one

---
+++ dbschema.js
@@ -0,0 +1,11 @@
+let db = {
+    screams: [
+        {
+            userHandle: 'user',
+            body: 'this is the scream body',
+            createdAt: "2020-07-03T10:54:10.755Z",
+            likeCount: 5,
+            commentCount: 2
+        }
+    ]
+}
